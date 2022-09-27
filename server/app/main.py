c8f12f06865f74bcc2b1816d26399b831f0193ce
crpier42 <crpier42@gmail.com>
2022-09-27 19:01:24 +0300

    fix fix periodic wipe but don't enable it

--- server/app/main.py
+++ server/app/main.py
@@ -10,18 +10,19 @@ from app import services
 
 async def periodic_wipe(persistence_service: services.StateService):
     while True:
+        await asyncio.sleep(1800)
         print("Wiping")
         persistence_service.__init__()
-        await asyncio.sleep(1800)
+        # persistence_service.populate_world_for_development()
 
 
 async def main():
     port = int(os.getenv("PORT", 8000))
     print(f"Listening on port {port}")
-    loop = asyncio.get_event_loop()
+    # loop = asyncio.get_event_loop()
     persistence_service = services.StateService()
     # TODO: this should depend on env var I think
-    persistence_service.populate_world_for_development()
+    # persistence_service.populate_world_for_development()
     # task = loop.create_task(periodic_wipe(persistence_service=persistence_service))
     conn_handler = services.ConnectionService(persistence_service=persistence_service)
 
